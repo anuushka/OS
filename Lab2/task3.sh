00b226505e91a315b291e44c70b096f53a636595
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-14 22:22:39 +0800

    Update task3.sh

--- Lab2/task3.sh
+++ Lab2/task3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -Ao pid,start --sort=start_time | tail -n 1 | awk '{print "PID: " $1, "start:" $2 }' > task3.out
+ps -Ao pid,start --sort=start_time | tail -n 1 | awk '{print "PID: " $1, "start: " $2 }' > task3.out
