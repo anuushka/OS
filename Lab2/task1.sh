d2f43902bd87fb30b658474b4215ad78506118a7
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-14 19:39:05 +0800

    Update task1.sh

--- Lab2/task1.sh
+++ Lab2/task1.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-ps -aux | wc -l > user.txt
-ps -aux | awk '{print $2 ":" $11}' >> user.txt
+# a selecting all processes
+# u selecting all processes, including those of other users
+# x select all processes without controlling types
+# ps -o user,pid,command --> list each column of processes
+
+#ps -aux > user.txt
+#awk '{if ($1 == "user") {print $0}}' user.txt >> user.txt
+
+#ps -aux | wc -l > user.txt
+#ps -aux | awk '{print $2 ":" $11}' >> user.txt
+
+ps -U user -u user --no-headers -o pid,command | awk '{print $1 ":" $2}' > task1.out
+echo Count: $(cat task1.out | wc -l) >> task1.out
+
+
+
