3702de4e303acc4846c1f5e1199a9f9e6f099d93
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-17 19:34:04 +0800

    Update test2_1.sh

--- Lab tests/test2_1.sh
+++ Lab tests/test2_1.sh
@@ -2,7 +2,7 @@
 ps -eo pid,etimes | awk '{if ($2 <= 60) print $1 " : " $2}'
 
 #Вывести список pid всех спящих процессов. Процесс работает не больше минуты.
-ps h -eo s,pid,etimes | awk '{ if (($1 == "S" || $1 == "D") && $3 <= 60) { print $1 ":" $2 } }'
+ps h -eo s,pid,etimes | awk '{ if (($1 == "S" || $1 == "D") && $3 <= 60) { print $1 ":" $2 ":" $3 } }'
 
 #Вывести топ 3 процессов с наибольшим использованием CPU, Процессы запущены от root (maybe.. sth like this)
 ps -Ao user,pid,pcpu --sort=-pcpu |  awk '{if ($2 == "root" print $1 ":" $3}' | head -n 4 
