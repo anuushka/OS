25dc83e64f002d49c86dc3878da7e194c6bfc442
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-17 19:43:09 +0800

    Update test2_1.sh

--- Lab tests/test2_1.sh
+++ Lab tests/test2_1.sh
@@ -7,3 +7,5 @@ ps h -eo s,pid,etimes | awk '{ if (($1 == "S" || $1 == "D") && $3 <= 60) { print
 #Вывести топ 3 процессов с наибольшим использованием CPU, Процессы запущены от root (maybe.. sth like this)
 ps -Ao user,pid,pcpu --sort=-pcpu |  awk '{if ($2 == "root" print $1 ":" $3}' | head -n 4 
 
+top -b -o +VIRT | head -n 8 | tail -n 2 >> task6.out
+
