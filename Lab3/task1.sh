e94c5853851b0fbfab0198b73310b43b14a7b8be
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-28 20:23:26 +0800

    Update task1.sh

--- Lab3/task1.sh
+++ Lab3/task1.sh
@@ -2,6 +2,6 @@
 
 now=$(date '%y.%m.%d.%H.%M.%S')
 report=~/lab3/report
-mkdir $HOME/test && echo "Catalog test was created succesfully" > $report && touch ~/lab3/$now
-ping -c1 www.net_nikogo.ru && echo "Verified" || echo "$now Host Down " >> $report
+(mkdir $HOME/test && echo "Catalog test was created succesfully" > $report && touch ~/lab3/$now) || echo "Unsuccesful" >> $report
+(ping -c1 www.net_nikogo.ru && echo "Verified" >> $report) || echo "$now Host Down " >> $report
 rm -r test
