aa6f43aa19b274674980c7a83bb2c3a5aa6b2427
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-29 01:14:32 +0800

    Update task3.sh

--- Lab3/task3.sh
+++ Lab3/task3.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-to list crontab task: crontab -l
-to remove crontab task: crontab -r
+#to list crontab task: crontab -l
+#to remove crontab task: crontab -r
+#show crontab: crontab -e
+# * * * * * : minute hour day of month month day
+#Run command at 15:00 everyday from Mon-Fri : 0 15 * * 1-5 command
+
+echo "*/5 * * * 6 ~/lab3/task1.sh" >> tmp
+crontab tmp
