d4cdcc5883f533f07721cf3a1816623c4451a2ff
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-28 23:56:05 +0800

    Update task2.sh

--- Lab3/task2.sh
+++ Lab3/task2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sleep 3
+#sleep 3
+#tail -n 0 -f ~/lab3/report
+#./task1.sh&
+
+#should use at not sleep - at – осуществляет однократный отсроченный запуск команды. 
+
+at now +2 minutes -f ./task1.sh
 tail -n 0 -f ~/lab3/report
-./task1.sh&
