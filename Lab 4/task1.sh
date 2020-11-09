1feaa580452ff903284ed565d6a4c2aa3019289d
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-11-09 20:57:01 +0800

    Create task1.sh

---
+++ Lab 4/task1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dir=$HOME/trash
+log=$HOME/trash.log
+file=$1
+id=$(uuidgen)
+
+if [[ $# -ne 1 ]]
+then
+echo "invalid argument"
+exit 1
+fi
+
+if [[ ! -f $file ]]
+then
+echo "invalid file"
+exit 1
+fi
+
+if [[ ! -d $dir ]]
+then 
+mkdir $dir
+touch $log
+fi
+
+ln $file $dir/$id
+rm $file && echo "Filename: $file   Path: $(realpath $file)   Link: $id" >> $log
+
